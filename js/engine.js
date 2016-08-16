56d3cc09c2153e61441887134068f01187f8157b
Matheus Henrique Ferri <Matheus Henrique Ferri>
2016-08-16 14:18:34 -0300

    Style concluído

--- js/engine.js
+++ js/engine.js
@@ -27,7 +27,8 @@ var Engine = (function(global) {
 
     canvas.width = 505;
     canvas.height = 909;
-    doc.body.appendChild(canvas);
+    $(".game").append(canvas);
+    
 
     /* This function serves as the kickoff point for the game loop itself
      * and handles properly calling the update and render methods.
